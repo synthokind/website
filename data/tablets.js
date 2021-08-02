461be1e1d2244a75701421c2ff2c6a23d13adf97
vikas-ranjan <88148134+vikas-ranjan@users.noreply.github.com>
2021-08-02 22:38:44 +0530

    Website add on

---
+++ data/tablets.js
@@ -0,0 +1,66 @@
+var tablets = [
+
+    {
+        id: '001',
+        name: 'ACLOFIT-PLUS (BLISTEP, l0x2x10)'
+    },
+    {
+        id: '002',
+        name: 'ACLOFIT-PLUS (ZEPODOL)'
+    },
+    {
+        id: '001',
+        name: 'ACLOFIT-PLUS (ALU-ALU)'
+    },
+    {
+        id: '001',
+        name: 'ACLOFIT-SP'
+    },
+    {
+        id: '001',
+        name: 'ACLOFIT-MR'
+    },
+    {
+        id: '001',
+        name: 'ACLOFIT-TH'
+    },
+    {
+        id: '001',
+        name: 'ALL 2 Z'
+    },
+    {
+        id: '001',
+        name: 'AMLOFIT-AT'
+    },
+    {
+        id: '001',
+        name: 'AZOBE-250 LB'
+    },
+    {
+        id: '001',
+        name: 'A7OBE-500 LB'
+    },
+    {
+        id: '001',
+        name: 'CEFOCET-IOO'
+    },
+    {
+        id: '001',
+        name: 'CEFOCET-200'
+    },
+    {
+        id: '001',
+        name: 'CEFOCET CV 325'
+    },
+    {
+        id: '001',
+        name: 'CEPOVIS-O'
+    }
+
+
+]
+
+
+
+
+export default tablets
